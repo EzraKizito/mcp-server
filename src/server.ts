27ae671ea3ddc59052a894d20bce09d78802eaf3
Ezra Sikuta Kizito <ezra.s.kizito@gmail.com>
2025-09-24 00:59:49 -0400

    Added first tool. Able to create a new user in a mock Database.

---
+++ src/server.ts
@@ -0,0 +1,72 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import fs from "node:fs/promises";
+
+// Create a server
+const server = new McpServer({
+  name: "test",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+    prompts: {},
+  },
+});
+
+server.tool(
+  "create-user",
+  "Create a new user in the database",
+  {
+    name: z.string(),
+    email: z.string(),
+    address: z.string(),
+    phone: z.string(),
+  },
+  {
+    title: "Create User",
+    readOnlyHint: true,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+  },
+  async (params) => {
+    try {
+      const id = await createUser(params);
+      return {
+        content: [{ type: "text", text: `User ${id} created successfully` }],
+      };
+    } catch {
+      return {
+        content: [{ type: "text", text: `Failed to save user` }],
+      };
+    }
+  }
+);
+
+async function createUser(user: {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+}) {
+  const users = await import("src/data/users.json", {
+    with: { type: "json" },
+  }).then((m) => m.default);
+
+  const id = users.length + 1;
+
+  users.push({ id, ...user });
+
+  await fs.writeFile("src/data/users.json", JSON.stringify(users, null, 2));
+
+  return id;
+}
+
+async function main() {
+  // Start the server
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+main();
